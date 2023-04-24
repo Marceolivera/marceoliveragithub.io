43da610fdb3da6218820087ab1a1527dfdb60633
Marcela Olivera <marceolivera.zu@gmail.com>
2023-04-23 21:56:22 -0300

    mi segundo commit

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#navbar'
+  })
